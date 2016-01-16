b9300d8138d6545f93f4024a2b06959c6a40081f
sai007 <sai_praneeth7777@yahoo.co.in>
2016-01-16 20:16:04 +0530

    changed login UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,31 @@
 @extends('layouts/header')
-
+<style type="text/css">
+#form{
+	background-color:black;
+	border-radius: 40px;
+	border: 3px black solid;
+	color:#308db9;
+	opacity: 0.8;
+	box-shadow: 3px 3px 13px 6px white;
+	position: absolute;
+	text-align: center;
+	min-height: 200px;
+	margin-top: 100px;
+}
+</style>
 @section('Content')
+<form id="form" class="col-lg-4 col-lg-offset-3">
+	<h2 style="opacity:1;text-shadow: 2px 2px #102e3c;">Welcome to IITM Profile Plus</h2>
 	{!! Form::open(array('method'=>'POST')) !!}
+	<div class="form-group" >
 		{!! Form::label('Username','Username') !!}
 		{!! Form::text('Username') !!}
+	</div>
+	<div class="form-group" >
 		{!! Form::label('Password','Password') !!}
 		{!! Form::password('Password') !!}
+	</div>
 		{!! Form::submit('Submit') !!}
 	{!! Form::close() !!}
+</form>
 @endsection
